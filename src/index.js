73097988e0940db75b37be47fdc4257a15338081
rusty-reebs <rusty_reebs@tutanota.com>
2021-08-12 12:29:26 +0400

    linked stylesheet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hi, testing")
+// TODO import and call init function from "initial.js" module
+// TODO put contents of each tab inside own module
+// TODO write tab-swtiching logic inside index.js.
+
+
+import { initialize } from "./components/initial";
+
+initialize();

eeb3de5b559b1957066ae96071e1ee793f656714
rusty-reebs <rusty_reebs@tutanota.com>
2021-08-16 17:02:30 +0400

    added image and testimonials

--- src/components/home.js
+++ src/components/home.js
@@ -5,19 +5,34 @@ const home = () => {
     const homeContainer = document.createElement("div");
     homeContainer.classList.add("homecontainer");
     const tagline = document.createElement("h3");
-    tagline.innerHTML = "Gamarjoba! All your favorite authentic Georgian recipes, made with pride in my kitchen!"
+    tagline.innerHTML = "Gamarjoba! We have all your favorite authentic Georgian recipes, made with pride in my kitchen!"
     container.appendChild(homeContainer);
     homeContainer.appendChild(tagline);
 
+    const supra = document.createElement("img");
+    supra.src = "../img/old-interior.jpeg";
+    supra.height = "333";
+    supra.width = "500";
+    homeContainer.appendChild(supra);
+
+    const testimonials = document.createElement("h3");
+    testimonials.innerHTML = "What people are saying about Nina's Restaurant:";
+    homeContainer.appendChild(testimonials);
+
+    const testiOne = document.createElement("h2");
+    testiOne.innerHTML = "Nina's has the best food I've tasted in Georgia! The khinkhali was outstanding! - Russell, Canada";
+    homeContainer.appendChild(testiOne);
+    const testiTwo = document.createElement("h2");
+    testiTwo.innerHTML = "I'm a local, and the food here is like my grandma's cooking. You'll find true Georgian taste and hospitality at Nina's. - Giorgi, Georgia";
+    homeContainer.appendChild(testiTwo);
+
+
+
 // TODO add testimonials and more copy
 
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Nina's Georgian Restaurant";
-// const tagline = document.createElement("p");
-// tagline.innerHTML = "Come enjoy some delicious Georgian food!";
+// We use only the freshest ingredients!
+// Perfect for every ocassion! We're happy to serve large groups!
 
-// content.appendChild(heading);
-// // content.appendChild(tagline);
 };
 
 export { home };
